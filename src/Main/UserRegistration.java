407af2ecfb88d382109303b637ee3dba7fb2cb40
Akshay Barve <akshaybarve06@gmail.com>
2020-03-23 11:11:00 +0530

    Added: Email Validation and Test Case

--- src/Main/UserRegistration.java
+++ src/Main/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     String namePattern="^[A-Z][a-zA-Z]{2,}$";
+    String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
 
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
@@ -13,4 +14,8 @@ public class UserRegistration {
     public boolean checkName(String name) {
         return Pattern.matches(namePattern,name);
     }
+    public boolean checkEmail(String email) {
+        return Pattern.matches(emailPattern,email);
+    }
+
 }
--- src/Test/UserRegistrationTest.java
+++ src/Test/UserRegistrationTest.java
@@ -6,29 +6,46 @@ import Main.UserRegistration;
 
 public class UserRegistrationTest {
 
-    UserRegistration object = new UserRegistration();
-
+    UserRegistration object=new UserRegistration();
     @Test
-    public void userFirstName_ValidThenTrue() {
-        boolean firstName = object.checkName("SampleFirstName");
+    public void userFirstName_ValidThenTrue(){
+        boolean firstName=object.checkName("SampleFirstName");
         Assert.assertTrue(firstName);
     }
-
     @Test
-    public void userFirstName_InvalidThenFalse() {
-        boolean firstName = object.checkName("samplefirstname");
+    public void userFirstName_InvalidThenFalse(){
+        boolean firstName=object.checkName("samplefirstname");
         Assert.assertFalse(firstName);
     }
-
     @Test
-    public void userLastName_ValidThenTrue() {
-        boolean lastName = object.checkName("SampleLastName");
+    public void userLastName_ValidThenTrue(){
+        boolean lastName=object.checkName("SampleLastName");
         Assert.assertTrue(lastName);
     }
-
     @Test
-    public void userLastName_InvalidThenFalse() {
-        boolean lastName = object.checkName("samplelastname");
+    public void userLastName_InvalidThenFalse(){
+        boolean lastName=object.checkName("samplelastname");
         Assert.assertFalse(lastName);
+    }
+    @Test
+    public void email_ValidThenTrue() {
+        String validEmails[]={"abc.xyz@bl.co.in","abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com",
+                "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
+                "abc@gmail.com.com","abc+100@gmail.com"};
+        for(int index=0;index<validEmails.length;index++){
+            boolean email=object.checkEmail(validEmails[index]);
+            Assert.assertTrue(email);
+        }
+    }
+    @Test
+    public void email_InvalidThenFalse() {
+        String invalidEmails[]={"abc","abc()*@gmail.com","abc@.com.my","abc123@gmail.a",
+                "abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com",
+                "abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com",
+                "abc@gmail.com.1a","abc@gmail.com.aa.au"};
+        for(int index1=0;index1<invalidEmails.length;index1++) {
+            boolean Email = object.checkEmail(invalidEmails[index1]);
+            Assert.assertFalse(Email);
+        }
     }
 }
